5b217a8d744739d3274ca2197f5171c65ebb619d
Eric Hennigan <eric.hennigan@gmail.com>
2013-11-27 09:27:53 -0800

    add zsh customization for prod-init-01

---
+++ etc/zsh/prod-init-01.zsh
@@ -0,0 +1,22 @@
+
+export PATH="/opt/rave/lib:/opt/rave/client:$PATH"
+
+alias rave-power="/opt/rave/client/PwrControl"
+alias rave-network="/opt/rave/lib/networkConfig"
+
+function rave-checkout {
+  svn checkout svn://ravesvn.ims.dom/Rave/projects/trunk $1/projects
+}
+
+function rave-ssh {
+  ssh root@172.168.0.10
+}
+
+function rave-start-servers {
+  /opt/rave/kraken_server/app_kraken_server.py -v sdu_audio_fixture -l /home/erich/krakenLogs/sdu_audio_fixture.log&
+  /opt/rave/kraken_server/app_kraken_server.py -v phidget -l /home/erich/krakenLogs/phidget.log&
+}
+
+function rave-start-client {
+  app_kraken_client.py -g qt sdu $2 --title "TESTING" --timeout 120 --lru $1 -v -l /home/erich/krakenLogs/client_sdu.log --power phidget
+}
