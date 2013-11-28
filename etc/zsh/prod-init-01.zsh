a92d5244a24f7e7fc97cabb0b75e61ff6014fcd1
Eric Hennigan <eric.hennigan@gmail.com>
2013-11-27 16:23:20 -0800

    Update the prod-init-01 zsh script for usability

--- etc/zsh/prod-init-01.zsh
+++ etc/zsh/prod-init-01.zsh
@@ -2,7 +2,7 @@
 export PATH="/opt/rave/lib:/opt/rave/client:$PATH"
 
 alias rave-power="/opt/rave/client/PwrControl"
-alias rave-network="/opt/rave/lib/networkConfig"
+alias rave-network="/opt/rave/lib/networkConfig.py"
 
 function rave-checkout {
   svn checkout svn://ravesvn.ims.dom/Rave/projects/trunk $1/projects
@@ -13,10 +13,13 @@ function rave-ssh {
 }
 
 function rave-start-servers {
+  sudo ls >/dev/null || return
   /opt/rave/kraken_server/app_kraken_server.py -v sdu_audio_fixture -l /home/erich/krakenLogs/sdu_audio_fixture.log&
   /opt/rave/kraken_server/app_kraken_server.py -v phidget -l /home/erich/krakenLogs/phidget.log&
 }
 
 function rave-start-client {
-  app_kraken_client.py -g qt sdu $2 --title "TESTING" --timeout 120 --lru $1 -v -l /home/erich/krakenLogs/client_sdu.log --power phidget
+  sudo touch kraken_debug.log
+  sudo chmod a+w kraken_debug.log
+  ./app_kraken_client.py -g qt sdu $2 --title "TESTING" --timeout 120 --lru $1 -v -l /home/erich/krakenLogs/client_sdu.log --power phidget
 }
