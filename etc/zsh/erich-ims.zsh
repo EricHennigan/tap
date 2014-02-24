479fd75d2b5f6ea7ce0f84eeed5e741eb9e2a959
Eric Hennigan <eric.hennigan@gmail.com>
2014-02-24 08:38:50 -0800

    add rave-patch helper function

--- etc/zsh/erich-ims.zsh
+++ etc/zsh/erich-ims.zsh
@@ -9,10 +9,6 @@ export MANPATH="$texdir/texmf-dist/doc/man:$INFOPATH"
 # screen config functions
 #----------------
 
-function rave-checkout {
-  svn checkout svn://ravesvn.ims.dom/Rave/projects/trunk $1/projects
-}
-
 function towork {
   optirun true
   sleep 1
@@ -39,8 +35,54 @@ if [[ -f "$HOME/.svnkit" ]]; then
     #export SVNME=svn://ravesvn/Rave/projects/branches/private/erich
 fi
 
+function rave-checkout {
+  svn checkout svn://ravesvn.ims.dom/Rave/projects/trunk $1/projects
+}
+
 function rave-update-svnkit {
     svn export svn://ravesvn/Rave/tools/svnkit/install.sh /tmp/svnkit-install.sh
     cd /tmp
     ./svnkit-install.sh
+}
+
+function rave-patch {
+    local ravedir=$HOME/work/$(pwd | sed -e's/.*rave/rave/' -e's/\/.*//')
+    echo "ravedir = $ravedir"
+    if [[ ! -d "$ravedir" ]]; then
+        echo "Could not find a rave directory by the name $ravedir";
+        return
+    fi
+
+    pushd "$ravedir"
+        # verify that remote diff exists
+        local remotediff=$(ls -t $HOME/tap/mnt/imsmfg/.$ravedir/*.diff | head -n1)
+        if [[ -z "$remotediff" ]] ; then
+            echo "Could not find remote diff"
+            popd
+            return
+        fi
+
+        # copy over the remote diff should one not yet exist (first time work setup)
+        echo "remotediff = $remotediff"
+        if [[ -z $(ls *.diff) ]]; then
+            cp $remotediff ./
+        fi
+
+        # verify that a local diff exists
+        local diff=$(ls -t *.diff | head -n1)
+        echo "diff = $diff"
+        if [[ -z "$diff" ]]; then
+            echo "Could not find a diff file to apply";
+            popd
+            return
+        fi
+
+        pushd projects
+            grep Index "../$diff" | field -1 -d' ' | xargs rm
+            svn up
+            \cp "$remotediff" ../
+            patch -p0 < "../$diff"
+            svn stat | grep -v '^?'
+        popd
+    popd > /dev/null
 }
