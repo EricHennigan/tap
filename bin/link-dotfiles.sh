920f7de984c2665fc8b54c18a76091bcae0d04ba
Eric Hennigan <eric.hennigan@gmail.com>
2014-08-18 20:30:45 -0700

    add redshift config file

--- bin/link-dotfiles.sh
+++ bin/link-dotfiles.sh
@@ -9,6 +9,13 @@ ln -sf tap/etc/vim/vimrc .vimrc
 ln -sf tap/etc/gitconfig .gitconfig
 ln -sf tap/etc/tmux.conf .tmux.conf
 
+ln -sf tap/etc/ssh/config .ssh/config
+
+mkdir -p $HOME/.config
+for file in $HOME/tap/config ; do
+    ln -sf $file $HOME/.config/
+done
+
 # setup zsh for this computer
 cd $HOME/tap/etc/zsh
 touch $(hostname).zsh
