edf1b95773bfbc9aae4842f6741d7f9eefed08a8
Eric Hennigan <eric.hennigan@gmail.com>
2021-10-21 03:15:05 +0000

    Update vim bundles and zsh inits

--- etc/zsh/googlers.com.zsh
+++ etc/zsh/googlers.com.zsh
@@ -3,8 +3,8 @@ source /etc/bash_completion.d/g4d
 set show-all-if-ambiguous on
 
 # appengine
-source $HOME/software/google-cloud-sdk/completion.zsh.inc
-source $HOME/software/google-cloud-sdk/path.zsh.inc
+# source $HOME/software/google-cloud-sdk/completion.zsh.inc
+# source $HOME/software/google-cloud-sdk/path.zsh.inc
 
 export PATH="/usr/lib/google-dartlang/bin:${PATH}"
 #export PATH="$HOME/software/go_appengine:${PATH}"
@@ -13,7 +13,9 @@ export PATH="${GOPATH}/bin:${PATH}"
 
 alias gd=g4d
 alias g=g4
+alias irabbit='iblaze -iblaze_blaze_binary rabbit'
 alias snippets="/google/data/ro/projects/contentads/adx/verification/tools/weekly_snippets"
+alias gpaste="/google/src/head/depot/eng/tools/pastebin"
 
 function gdb() { cd $(echo $(pwd) | sed "s,google3/,google3/blaze-bin/,"); }
 function gdc() { cd $(echo $(pwd) | sed "s,google3/(blaze-bin/)?,google3/,"); }
@@ -21,6 +23,7 @@ function gdjt() { cd $(echo $(pwd) | sed "s,google3/(blaze-bin/|java/)?,google3/
 function gdj() { cd $(echo $(pwd) | sed "s,google3/(blaze-bin/|javatests/)?,google3/java/,"); }
 
 alias blt="blaze test --flaky_test_attempts=1"
+alias jadep="/google/data/ro/teams/jade/jadep"
 
 function myblaze() {
   iblaze $@ --test_output=streamed 2>&1 | grep -A5 '^([0-9]+\)|INFO|DEBUG|THOTH|Caused by)'
