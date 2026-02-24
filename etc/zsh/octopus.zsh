7793233706f42e33812259484be17e2d2944e27f
Eric Hennigan <eric.hennigan@gmail.com>
2026-02-23 23:16:50 -0800

    Add Opencode and CUDA environments

--- etc/zsh/octopus.zsh
+++ etc/zsh/octopus.zsh
@@ -1,2 +1,6 @@
+# CUDA (but this failed, because radeon)
 export PATH=/usr/local/cuda/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+
+# Opencode https://opencode.ai
+export PATH=$HOME/.opencode/bin:$PATH
